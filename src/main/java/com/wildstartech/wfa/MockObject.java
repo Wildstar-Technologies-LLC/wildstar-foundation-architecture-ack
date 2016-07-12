f9b42faa649b23f8849e4a99de4c488dbfa1480a
Derek Berube <derek.berube@wildstartech.com>
2016-07-12 11:16:22 -0500

    Adding the com.wildstartech.wfa.MockObject.java file back into the code base.

---
+++ src/main/java/com/wildstartech/wfa/MockObject.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2001 - 2016 Wildstar Technologies, LLC.
+ *
+ * This file is part of the Wildstar Foundation Architecture ACK.
+ *
+ * Wildstar Foundation Architecture Application Compatibility Kit (WFA-ACK) 
+ * is free software: you can redistribute it and/or modify it under the 
+ * terms of the GNU General Public License as published by the Free  
+ * Software Foundation, either version 3 of the License, or (at your  
+ * option) any later version.
+ *
+ * WFA-ACK is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * WFA-ACK.  If not, see  <http://www.gnu.org/licenses/>.
+ * 
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the 
+ * GNU General Public License cover the whole combination.
+ * 
+ * As a special exception, the copyright holders of this library give you 
+ * permission to link this library with independent modules to produce an 
+ * executable, regardless of the license terms of these independent modules, 
+ * and to copy and distribute the resulting executable under terms of your 
+ * choice, provided that you also meet, for each linked independent module, the
+ * terms and conditions of the license of that module. An independent module is
+ * a module which is not derived from or based on this library. If you modify 
+ * this library, you may extend this exception to your version of the library, 
+ * but you are not obliged to do so. If you do not wish to do so, delete this 
+ * exception statement from your version.
+ * 
+ * If you need additional information or have any questions, please contact:
+ *
+ *      Wildstar Technologies, LLC.
+ *      63 The Greenway Loop
+ *      Panama City Beach, FL 32413
+ *      USA
+ *
+ *      derek.berube@wildstartech.com
+ *      www.wildstartech.com
+ */
+package com.wildstartech.wfa;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MockObject {
+	// ********** Utility methods
+	/**
+	 * 
+	 */
+	public String defaultValue(String value) {
+		String returnValue="";
+		if (value != null) {
+			returnValue=value;
+		} // END if (value != null)
+		return returnValue;
+	}
+	
+	/**
+	 * Parses a string value to return a {@code Date}
+	 * 
+	 * @param parseableDate
+	 *            A string that represents a date expressed using the following
+	 *            format pattern: "MM/dd/yyyy hh:mm a".
+	 * @return A {@code Date} object that corresponds to the {@code String}
+	 *         value passed as a parameter.
+	 */
+	public Date parseDate(String parseableDate) {
+		Date returnDate = null;
+		DateFormat fmt = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+		try {
+			returnDate = fmt.parse(parseableDate);
+		} catch (ParseException ex) {
+
+		} // END try/catch
+		return returnDate;
+	}
+}
